42e069e2fe45b0789e9ed34bcc77b5748d7c5e87
Alex Campos Arcos <alex_campos_arcos@hotmail.com>
2017-03-21 16:29:42 -0300

    Add project files.

---
+++ devsolution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(devsolution.Startup))]
+namespace devsolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
